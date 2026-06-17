3eea0b720cf0a730d9a1579cc785481acb4491e2
MaDongdong <donaldma@tencent.com>
2026-06-17 18:56:56 +0800

    feat: support l4 and l5 org nodes in department module (backward compatible)

--- modules/tco/department/main.tf
+++ modules/tco/department/main.tf
@@ -39,6 +39,48 @@ locals {
   l3_nodes = { for l3 in local.l3_node_list: l3.k => l3 }
   l3_node_ids = { for k, node in tencentcloud_organization_org_node.l3_nodes: k => node.id }
 
+  l4_node_list = flatten(
+    [
+      for l1_k, l1 in var.l1_org_nodes: [
+        for l2_k, l2 in (try(l1.sub_nodes, null) != null ? l1.sub_nodes : {}): [
+          for l3_k, l3 in (try(l2.sub_nodes, null) != null ? l2.sub_nodes : {}): [
+            for l4_k, l4 in (try(l3.sub_nodes, null) != null ? l3.sub_nodes : {}): {
+              "l3_k"   = l3_k
+              "k"      = l4_k
+              "name"   = try(l4.name, l4_k)
+              "remark" = try(l4.remark, "")
+              "tags"   = try(l4.tags, {})
+            }
+          ]
+        ]
+      ] if var.create
+    ]
+  )
+  l4_nodes = { for l4 in local.l4_node_list: l4.k => l4 }
+  l4_node_ids = { for k, node in tencentcloud_organization_org_node.l4_nodes: k => node.id }
+
+  l5_node_list = flatten(
+    [
+      for l1_k, l1 in var.l1_org_nodes: [
+        for l2_k, l2 in (try(l1.sub_nodes, null) != null ? l1.sub_nodes : {}): [
+          for l3_k, l3 in (try(l2.sub_nodes, null) != null ? l2.sub_nodes : {}): [
+            for l4_k, l4 in (try(l3.sub_nodes, null) != null ? l3.sub_nodes : {}): [
+              for l5_k, l5 in (try(l4.sub_nodes, null) != null ? l4.sub_nodes : {}): {
+                "l4_k"   = l4_k
+                "k"      = l5_k
+                "name"   = try(l5.name, l5_k)
+                "remark" = try(l5.remark, "")
+                "tags"   = try(l5.tags, {})
+              }
+            ]
+          ]
+        ]
+      ] if var.create
+    ]
+  )
+  l5_nodes = { for l5 in local.l5_node_list: l5.k => l5 }
+  l5_node_ids = { for k, node in tencentcloud_organization_org_node.l5_nodes: k => node.id }
+
   discrete_node_ids = { for k, node in tencentcloud_organization_org_node.discrete_nodes: k => node.id}
 }
 
@@ -62,6 +104,22 @@ resource "tencentcloud_organization_org_node" "l3_nodes" {
   for_each = local.l3_nodes
   name           = each.value.name
   parent_node_id = local.l2_node_ids[each.value.l2_k]
+  remark         = try(each.value.remark, "")
+  tags           = try(each.value.tags, {})
+}
+
+resource "tencentcloud_organization_org_node" "l4_nodes" {
+  for_each = local.l4_nodes
+  name           = each.value.name
+  parent_node_id = local.l3_node_ids[each.value.l3_k]
+  remark         = try(each.value.remark, "")
+  tags           = try(each.value.tags, {})
+}
+
+resource "tencentcloud_organization_org_node" "l5_nodes" {
+  for_each = local.l5_nodes
+  name           = each.value.name
+  parent_node_id = local.l4_node_ids[each.value.l4_k]
   remark         = try(each.value.remark, "")
   tags           = try(each.value.tags, {})
 }
--- modules/tco/department/outputs.tf
+++ modules/tco/department/outputs.tf
@@ -10,6 +10,14 @@ output "l3_nodes" {
   value = local.l3_node_ids
 }
 
+output "l4_nodes" {
+  value = local.l4_node_ids
+}
+
+output "l5_nodes" {
+  value = local.l5_node_ids
+}
+
 output "discrete_nodes" {
   value = local.discrete_node_ids
 }
